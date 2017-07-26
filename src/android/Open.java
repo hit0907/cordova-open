efbc583d378f2f1d36e1373fde9a1c05b57a6eca
Hanh LD <duchanhpy2000@gmail.com>
2017-07-26 11:37:24 +0700

    Update Open.java

--- src/android/Open.java
+++ src/android/Open.java
@@ -1,16 +1,22 @@
 package com.disusered;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
+import android.webkit.MimeTypeMap;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.net.Uri;
-import android.content.Intent;
-import android.webkit.MimeTypeMap;
-import android.content.ActivityNotFoundException;
-import android.os.Build;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 /**
  * This class starts an activity for an intent to view files
@@ -41,10 +47,8 @@ public class Open extends CordovaPlugin {
     String extension = MimeTypeMap.getFileExtensionFromUrl(path);
     if (extension != null) {
       MimeTypeMap mime = MimeTypeMap.getSingleton();
-      mimeType = mime.getMimeTypeFromExtension(extension);
+      mimeType = mime.getMimeTypeFromExtension(extension.toLowerCase());
     }
-
-    System.out.println("Mime type: " + mimeType);
 
     return mimeType;
   }
@@ -58,22 +62,50 @@ public class Open extends CordovaPlugin {
   private void chooseIntent(String path, CallbackContext callbackContext) {
     if (path != null && path.length() > 0) {
       try {
-        Uri uri = Uri.parse(path);
+
         String mime = getMimeType(path);
+
+        path = java.net.URLDecoder.decode(path, "UTF-8");
+        Uri uri = Uri.parse(path);
+
         Intent fileIntent = new Intent(Intent.ACTION_VIEW);
 
-        if( Build.VERSION.SDK_INT > 15 ){
-          fileIntent.setDataAndTypeAndNormalize(uri, mime); // API Level 16 -> Android 4.1
+        // Android O above
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+
+          // Android 24 above not support path file:///
+          path = path.replace("file:/","");
+
+
+          File newFile = new File(path);
+
+          Uri contentUri = FileProvider.getUriForFile(cordova.getActivity().getApplicationContext(),
+                  cordova.getActivity().getPackageName() + ".fileProvider",
+                  newFile);
+
+          fileIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+          fileIntent.setDataAndType(contentUri, mime);
+
+          List<ResolveInfo> infoList = cordova.getActivity().getApplicationContext().getPackageManager().queryIntentActivities(fileIntent, PackageManager.MATCH_DEFAULT_ONLY);
+          for (ResolveInfo resolveInfo : infoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            cordova.getActivity().getApplicationContext().grantUriPermission(packageName, contentUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+          }
+
         } else {
           fileIntent.setDataAndType(uri, mime);
+          fileIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         }
 
         cordova.getActivity().startActivity(fileIntent);
-
+//        cordova.getActivity().startActivity(Intent.createChooser(fileIntent, "Open File in..."));
         callbackContext.success();
       } catch (ActivityNotFoundException e) {
         e.printStackTrace();
         callbackContext.error(1);
+      } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
       }
     } else {
       callbackContext.error(2);
